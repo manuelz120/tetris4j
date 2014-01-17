3a89d2bac771b9bf4dcb75da5369c70d1d0780da
Manuel Zametter <m.zametter@gmail.com>
2014-01-17 21:40:14 +0100

    Updated Ansi-Colors

--- src/at/tetris4j/resources/AnsiCodes.java
+++ src/at/tetris4j/resources/AnsiCodes.java
@@ -8,9 +8,29 @@ package at.tetris4j.resources;
 public class AnsiCodes {
 	public static final String ANSI_CLS = "\u001b[2J";
 	public static final String ANSI_HOME = "\u001b[H";
+	public static final String ANSI_REVERSEON = "\u001b[7m";
+	
 	public static final String ANSI_BOLD = "\u001b[1m";
 	public static final String ANSI_AT55 = "\u001b[10;10H";
-	public static final String ANSI_REVERSEON = "\u001b[7m";
 	public static final String ANSI_NORMAL = "\u001b[0m";
-	public static final String ANSI_WHITEONBLUE = "\u001b[37;44m";
+	
+	public static final String ANSI_FOREGROUND_BLACK = "\u001b[30";
+	public static final String ANSI_FOREGROUND_RED = "\u001b[31";
+	public static final String ANSI_FOREGROUND_GREEN = "\u001b[32";
+	public static final String ANSI_FOREGROUND_YELLOW = "\u001b[33";
+	public static final String ANSI_FOREGROUND_BLUE = "\u001b[34";
+	public static final String ANSI_FOREGROUND_MAGENTA = "\u001b[35";
+	public static final String ANSI_FOREGROUND_CYAN = "\u001b[36";
+	public static final String ANSI_FOREGROUND_WHITE = "\u001b[37";
+	
+	public static final String ANSI_BACKGROUND_BLACK = "\u001b[40";
+	public static final String ANSI_BACKGROUND_RED = "\u001b[41";
+	public static final String ANSI_BACKGROUND_GREEN = "\u001b[42";
+	public static final String ANSI_BACKGROUND_YELLOW = "\u001b[43";
+	public static final String ANSI_BACKGROUND_BLUE = "\u001b[44";
+	public static final String ANSI_BACKGROUND_MAGENTA = "\u001b[45";
+	public static final String ANSI_BACKGROUND_CYAN = "\u001b[46";
+	public static final String ANSI_BACKGROUND_WHITE = "\u001b[47";
+	
+	
 }
