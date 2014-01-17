90fe3649a089a039f06d7d10678b6aba307a766a
Manuel Zametter <m.zametter@gmail.com>
2014-01-17 21:50:00 +0100

    Added additional Ansi-Control-Codes

--- src/at/tetris4j/resources/AnsiCodes.java
+++ src/at/tetris4j/resources/AnsiCodes.java
@@ -7,6 +7,13 @@ package at.tetris4j.resources;
  */
 public class AnsiCodes {
 	public static final String ANSI_CLS = "\u001b[2J";
+	public static final String ANSI_DELETE_LINE = "\u001b[K";
+	public static final String ANSI_CURSOR_UP = "\u001b[A";
+	public static final String ANSI_CURSOR_DOWN = "\u001b[B";
+	public static final String ANSI_CURSOR_FORWARD = "\u001b[C";
+	public static final String ANSI_CURSOR_BACKWARD = "\u001b[D";
+	public static final String ANSI_SAVE_CURSORPOS = "\u001b[s";
+	public static final String ANSI_RESORE_CURSORPOS = "\u001b[u";
 	public static final String ANSI_HOME = "\u001b[H";
 	public static final String ANSI_REVERSEON = "\u001b[7m";
 	
@@ -32,5 +39,22 @@ public class AnsiCodes {
 	public static final String ANSI_BACKGROUND_CYAN = "\u001b[46";
 	public static final String ANSI_BACKGROUND_WHITE = "\u001b[47";
 	
-	
+	/**
+	 * Builds the Ansi-Code to set the cursor to any desired position.
+	 * @param line
+	 * The number of the line where you want to place the cursor.
+	 * @param column
+	 * The number of the line where you want to place the cursor.
+	 * @return
+	 * The Ansi-Code to set the cursor to any desired position.
+	 */
+	public static String ANSI_SET_CURSORPOS(int line, int column){
+		StringBuilder sb = new StringBuilder();
+		sb.append("\u001b[");
+		sb.append(line);
+		sb.append(';');
+		sb.append(column);
+		sb.append('H');
+		return sb.toString();
+	}
 }
